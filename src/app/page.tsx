21e3089cb62e03f1d9b96b9caaf9c5bc31384b91
Raul Zarza <raulzarza.dev@gmail.com>
2023-09-02 13:09:19 -0700

    now you use material ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,27 @@
+'use client'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import { Button, Grid, Stack } from '@mui/material'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  return <main>Bienvenidos</main>
+  return (
+    <main>
+      <Grid
+        container
+        height="100vh"
+        alignItems="center"
+        justifyContent="center"
+        direction="column"
+      >
+        <h1>Using Material UI with Next.js 13</h1>
+        <Stack direction="row" columnGap={1}>
+          <Button variant="text">Text</Button>
+          <Button variant="contained">Contained</Button>
+          <Button variant="outlined">Outlined</Button>
+        </Stack>
+      </Grid>
+    </main>
+  )
 }
