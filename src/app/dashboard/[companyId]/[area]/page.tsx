a89276eab5857aadcd73b1d7033ab7ebb98cadce
Raul Zarza <raulzarza.dev@gmail.com>
2023-10-06 14:35:28 -0700

    refactor(page area): now you dont use objetct to determinate the component

--- src/app/dashboard/[companyId]/[area]/page.tsx
+++ src/app/dashboard/[companyId]/[area]/page.tsx
@@ -10,44 +10,12 @@ import ErrorBoundary from '@/components/ErrorBoundary'
 import { StaffPermission, StaffPermissionLabels } from '@/types/staff'
 import { Container, Typography } from '@mui/material'
 
-const component: Record<
-  StaffPermission,
-  { label?: string; icon: IconName; component: React.ReactNode }
-> = {
-  CASHBOX: {
-    icon: 'cashbox',
-    component: <CompanyCashbox />
-  },
-  SALES: {
-    icon: 'money',
-    component: <CompanyStore />
-  },
-  MAINTENANCE: {
-    icon: 'fix',
-    component: <CompanyMaintenance />
-  },
-  DELIVERY: {
-    icon: 'delivery',
-    component: <CompanyDelivery />
-  },
-  ADMIN: {
-    icon: 'person',
-    component: <CompanyAdmin />
-  },
-  RECEPTION: {
-    icon: 'store',
-    component: <CompanyReception />
-  }
-}
 const Page = ({
   params
 }: {
   params: { area: StaffPermission; companyId: string }
 }) => {
   const { area } = params
-  const comp = component[area].component
-
-  if (!comp) return <>No component</>
 
   return (
     <ErrorBoundary componentName="Areas">
@@ -55,7 +23,13 @@ const Page = ({
         <Typography className="text-center text-xl font-bold my-4">
           {StaffPermissionLabels[area]}
         </Typography>
-        {comp}
+        {/* {comp} */}
+        {area === 'ADMIN' && <CompanyAdmin />}
+        {area === 'CASHBOX' && <CompanyCashbox />}
+        {area === 'DELIVERY' && <CompanyDelivery />}
+        {area === 'MAINTENANCE' && <CompanyMaintenance />}
+        {area === 'RECEPTION' && <CompanyReception />}
+        {area === 'SALES' && <CompanyStore />}
       </Container>
     </ErrorBoundary>
   )
