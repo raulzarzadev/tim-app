6e35306efe7799a9cf6d103c35e47a373b8027f4
Raul Zarza <raulzarza.dev@gmail.com>
2023-10-06 21:04:30 -0700

    feat(uploading file): now you can see a progress bar when uploading image

---
+++ src/components/CircularProgressLabel.tsx
@@ -0,0 +1,46 @@
+import * as React from 'react'
+import CircularProgress, {
+  CircularProgressProps
+} from '@mui/material/CircularProgress'
+import Typography from '@mui/material/Typography'
+import Box from '@mui/material/Box'
+
+function CircularProgressLabel(
+  props: CircularProgressProps & { value: number }
+) {
+  return (
+    <Box
+      sx={{
+        position: 'relative',
+        display: 'inline-flex',
+        justifyContent: 'center'
+      }}
+    >
+      <CircularProgress
+        variant="determinate"
+        color="secondary"
+        thickness={6}
+        size={50}
+        {...props}
+      />
+      <Box
+        sx={{
+          top: 0,
+          left: 0,
+          bottom: 0,
+          right: 0,
+          position: 'absolute',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center'
+        }}
+      >
+        <Typography variant="caption" component="div">{`${Math.round(
+          props.value
+        )}%`}</Typography>
+      </Box>
+    </Box>
+  )
+}
+
+export default CircularProgressLabel
--- src/components/InputUploadFile.tsx
+++ src/components/InputUploadFile.tsx
@@ -2,8 +2,8 @@ import * as React from 'react'
 import { styled } from '@mui/material/styles'
 import Button from '@mui/material/Button'
 import CloudUploadIcon from '@mui/icons-material/CloudUpload'
-import { FirebaseCRUD } from '@/firebase/firebase.CRUD'
 import { uploadFile } from '@/firebase/files'
+import CircularProgressLabel from './CircularProgressLabel'
 
 const VisuallyHiddenInput = styled('input')({
   clip: 'rect(0 0 0 0)',
@@ -24,24 +24,39 @@ export default function InputUploadFile({
   label: string
   setURL?: (url: string) => void
 }) {
+  const [progress, setProgress] = React.useState(0)
   const onUploadFile = (files: FileList | null) => {
+    setProgress(1)
     const file = files?.[0]
     if (!file) return console.error('first file is null')
     uploadFile(file, label, ({ progress, downloadURL }) => {
-      if (downloadURL) setURL?.(downloadURL)
+      setProgress(progress)
+      if (downloadURL) {
+        setURL?.(downloadURL)
+        setTimeout(() => {
+          setProgress(0)
+        }, 500)
+      }
     })
   }
+
+  const showProgressBar = progress > 0 || progress === 100
+
   return (
-    <Button
-      component="label"
-      variant="contained"
-      startIcon={<CloudUploadIcon />}
-    >
-      {label}
-      <VisuallyHiddenInput
-        type="file"
-        onChange={(e) => onUploadFile(e.target.files)}
-      />
-    </Button>
+    <>
+      <Button
+        component="label"
+        variant="contained"
+        startIcon={<CloudUploadIcon />}
+        disabled={showProgressBar}
+      >
+        <span className="mr-2">{label}</span>
+        {showProgressBar && <CircularProgressLabel value={progress} />}
+        <VisuallyHiddenInput
+          type="file"
+          onChange={(e) => onUploadFile(e.target.files)}
+        />
+      </Button>
+    </>
   )
 }
