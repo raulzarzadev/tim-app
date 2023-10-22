1644318d7073668811b05c2ec49643dd87b0c326
Raul Zarza <raulzarza.dev@gmail.com>
2023-10-21 23:40:33 -0700

    fix(servicestable): fix overflow table

--- src/components/MyTable.tsx
+++ src/components/MyTable.tsx
@@ -13,36 +13,38 @@ const MyTable = ({
   onRowClick?: (id: string) => void
 }) => {
   return (
-    <table className="w-full text-center max-w-full">
-      <thead>
-        <tr>
-          {data.headers.map((h) => (
-            <th key={h.key}>{h.label}</th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {data.body.map((b) => (
-          <tr
-            key={b?.id}
-            className={`${
-              onRowClick ? ' hover:bg-slate-200 cursor-pointer ' : ''
-            } `}
-            onClick={() => onRowClick?.(b.id)}
-          >
+    <div className="overflow-x-auto">
+      <table className="w-full text-center max-w-full ">
+        <thead>
+          <tr>
             {data.headers.map((h) => (
-              <td key={h.key} className="">
-                <span className=" ">
-                  {h.format
-                    ? h.format(get(b, h.key))
-                    : get(b, h?.key)?.toString() || '-'}
-                </span>
-              </td>
+              <th key={h.key}>{h.label}</th>
             ))}
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {data.body.map((b) => (
+            <tr
+              key={b?.id}
+              className={`${
+                onRowClick ? ' hover:bg-slate-200 cursor-pointer ' : ''
+              } `}
+              onClick={() => onRowClick?.(b.id)}
+            >
+              {data.headers.map((h) => (
+                <td key={h.key} className="">
+                  <span className=" ">
+                    {h.format
+                      ? h.format(get(b, h.key))
+                      : get(b, h?.key)?.toString() || '-'}
+                  </span>
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   )
 }
 
